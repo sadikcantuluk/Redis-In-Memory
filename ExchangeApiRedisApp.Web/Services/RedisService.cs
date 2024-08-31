9a5a48dc3f13d0444fa23539a9835c5ac49f3075
Sadikcan Tuluk <sadikcantuluk@gmail.com>
2024-08-31 16:58:45 +0300

    StackExchange.RedisAPI ile Redis yapısı kodlandı.

---
+++ ExchangeApiRedisApp.Web/Services/RedisService.cs
@@ -0,0 +1,19 @@
+﻿using StackExchange.Redis;
+
+namespace ExchangeApiRedisApp.Web.Services
+{
+    public class RedisService
+    {
+        ConnectionMultiplexer _connectionMultiplexer;
+        public void Connect() => _connectionMultiplexer = ConnectionMultiplexer.Connect("localhost:1453");
+        public IDatabase GetDb(int db) => _connectionMultiplexer.GetDatabase(db);
+    }
+}
+
+
+
+
+
+
+
+
